b38902034909f03bc816f6ba1d3823ec1a03d83d
attit-maharjan <81147776+attit-maharjan@users.noreply.github.com>
2025-03-14 12:23:54 +0100

    another_change

--- BI_VGS/urls.py
+++ BI_VGS/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('office.urls')),  # This connects to your app
+    path('office/', include('office.urls')),  # This connects to your app
 ]
