928f4456a9539aac3c30bcef0435a76066d78910
attit-maharjan <81147776+attit-maharjan@users.noreply.github.com>
2025-04-11 18:35:04 +0200

    logout working now

--- school/urls.py
+++ school/urls.py
@@ -5,6 +5,7 @@ from .views import custom_login_view, home_view
 urlpatterns = [
     path('', home_view, name='home'),  # Added home page URL
     path('login/', custom_login_view, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='school/login.html', next_page='login'), name='logout'),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='school/password_reset.html'), name='password_reset'),
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='school/password_reset_done.html'), name='password_reset_done'),
     path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='school/password_reset_confirm.html'), name='password_reset_confirm'),
